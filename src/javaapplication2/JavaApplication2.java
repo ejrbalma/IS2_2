d81c79a0a67d058aaf1c203541fa995a7890968e
Entrar <Entrar@10.22.145.214>
2017-10-16 14:14:08 +0100

    VERSION 3 --completa-STRING

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -20,7 +20,7 @@ public class JavaApplication2 {
     public static void main(String[] args) {
 
         //Integer [] vector = {1, 3, 5, 7, 9, 1, 5, 4, 3, 9,100,-4};
-         String[] vector = {"Ana", "Juan", "Elena", "Elena", "Juan", "Ana", "Pedro"};
+         String[] vector = {"Ana", "Juan", "Elena", "Elena", "Juan", "Ana", "Pedro","Ana", "Juan", "Elena",};
         
          Histogram <String> histogram = CalculaHistogram.computeHistogram(vector);
         
